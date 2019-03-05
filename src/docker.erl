346ba0e96fc55a081730663df33e24e9ed194b5d
Artem Teslenko <ates@ipv6.dp.ua>
2019-03-05 21:47:10 +0200

    Initial commit

---
+++ src/docker.erl
@@ -0,0 +1,60 @@
+-module(docker).
+
+-export([g/1]).
+-export([p/2]).
+-export([d/1]).
+
+g(Req) ->
+    make_req(get, Req, []).
+
+p(Req, Data) ->
+    make_req(post, Req, Data).
+
+d(Req) ->
+    make_req(delete, Req, []).
+
+make_req(Method, URI, Data) ->
+    case gun:open_unix(socket_path(), #{}) of
+        {ok, Pid} ->
+            StreamRef = send_req(Method, Pid, <<(api())/binary, URI/binary>>, Data),
+            Response = wait_response(Pid, StreamRef, <<>>),
+            gun:close(Pid),
+            Response;
+        Error -> Error
+    end.
+
+send_req(get, Pid, URI, _Data) ->
+    gun:get(Pid, URI);
+send_req(post, Pid, URI, Data) ->
+    gun:post(Pid, URI, [{<<"content-type">>, <<"application/json">>}], to_json(Data));
+send_req(delete, Pid, URI, _Data) ->
+    gun:delete(Pid, URI).
+
+socket_path() ->
+    application:get_env(?MODULE, socket, <<"/var/run/docker.sock">>).
+
+wait_response(Pid, StreamRef, Acc) ->
+    case gun:await(Pid, StreamRef) of
+        {response, nofin, _Status, _Headers} ->
+            wait_response(Pid, StreamRef, Acc);
+        {response, fin, Status, _Headers} ->
+            {ok, Status, Acc};
+        {data, nofin, Data} ->
+            wait_response(Pid, StreamRef, <<Acc/binary, Data/binary>>);
+        {data, fin, Data} ->
+            {ok, from_json(<<Acc/binary, Data/binary>>)};
+        Error -> Error
+    end.
+
+from_json(Data) ->
+    try
+        jsx:decode(Data, [return_maps])
+    catch
+        _:_Reason:_Stack ->
+            Data
+    end.
+
+to_json(Data) when is_map(Data) -> jsx:encode(Data);
+to_json(Data) when is_binary(Data) -> cow_uri:urlencode(Data).
+
+api() -> application:get_env(docker, version, <<"/v1.37">>).
